875c2ca29841bc8f059db9e0684283d8257d2b9b
Oleg Selivantsev <o.selivancev@gmail.com>
2024-05-04 18:20:26 +0300

    Update mongoConnect

--- backend/db/start.js
+++ backend/db/start.js
@@ -1,1 +1,19 @@
-console.log('start js')
+const {MongoClient} = require("mongodb");
+const {DB_URI} = require("./config")
+const mongoClient = new MongoClient(DB_URI);
+
+async function run() {
+    try {
+        const database = mongoClient.db('testdb');
+        const data = database.collection('testcollection');
+        const list = await data.find().toArray();
+        console.log('list', list);
+
+    } catch (err) {
+        console.log('DB error', err)
+    } finally {
+        await mongoClient.close();
+    }
+}
+
+run().catch(console.dir);
