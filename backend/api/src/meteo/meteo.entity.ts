e3ac690b98e69b555ecea594295acc21794cfc06
Oleg Selivantsev <o.selivancev@gmail.com>
2025-08-06 16:15:02 +0400

    meteo.entity.ts upd

--- backend/api/src/meteo/meteo.entity.ts
+++ backend/api/src/meteo/meteo.entity.ts
@@ -1,7 +1,7 @@
 import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
 
 @Entity({ name: 'meteo' })
-export class ArticleEntity {
+export class MeteoEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
