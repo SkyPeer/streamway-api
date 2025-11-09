0445f847c0d82d32bbe69b2cef4dc859d1bb7f4d
Oleg Selivantsev <o.selivancev@gmail.com>
2025-11-09 06:36:07 +0400

    refactoring app

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '@app/user/user.module';
-import {AuthMiddleware} from "@app/middlewares/auth.middleware";
-import {ArticleModule} from "@app/article/article.module";
+import { AuthMiddleware } from '@app/middlewares/auth.middleware';
+import { ArticleModule } from '@app/article/article.module';
 import ormconfig from '@app/ormconfig';
-import {ForecastModule} from "@app/forecast/forecast.module";
+import { ForecastModule } from '@app/forecast/forecast.module';
 
 // Examples api's requests
 // https://realworld-docs.netlify.app/
@@ -28,6 +28,6 @@ export class AppModule {
     consumer.apply(AuthMiddleware).forRoutes({
       path: '*',
       method: RequestMethod.ALL,
-    })
+    });
   }
 }
