c789cfa96c71c0157ef45b50dc0425f2da1cf3da
Oleg Selivantsev <o.selivancev@gmail.com>
2025-11-30 01:37:50 +0400

    upd format

--- backend/api/src/forecast/entities/tf_model.entity.ts
+++ backend/api/src/forecast/entities/tf_model.entity.ts
@@ -1,33 +1,42 @@
-import {Column, Entity, PrimaryGeneratedColumn,
-    CreateDateColumn, JoinColumn,
-    OneToOne, OneToMany} from 'typeorm';
-import {CityEntity} from "@app/forecast/entities/city.entity";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  JoinColumn,
+  OneToOne,
+  OneToMany,
+  ManyToOne,
+} from 'typeorm';
+import { CityEntity } from '@app/forecast/entities/city.entity';
+import { TF_training } from '@app/forecast/entities/tf_training.entity';
 
-@Entity({name: 'tf_models'})
+@Entity({ name: 'tf_models' })
 export class TFModel_Entity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({unique: true, nullable: false})
-    model_name: string;
+  @Column({ unique: true, nullable: false })
+  model_name: string;
 
-    @Column({default: ''})
-    model_topology: string;
+  @Column({ default: '' })
+  model_topology: string;
 
-    @Column({default: ''})
-    weight_specs: string;
+  @Column({ default: '' })
+  weight_specs: string;
 
-    @Column({type: 'bytea', nullable: true}) // or false, depending on your requirements
-    weights: Buffer; // Or Uint8Array, depending on your preference
+  @Column({ type: 'bytea', nullable: true }) // or false, depending on your requirements
+  weights: Buffer; // Or Uint8Array, depending on your preference
 
-    @Column({default: ''})
-    description: string;
+  @Column({ default: '' })
+  description: string;
 
-    @CreateDateColumn({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
-    updated_at: Date;
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated_at: Date;
 
-    @OneToOne(() => CityEntity, city=> city.tf_model)
-    city: CityEntity;
+  @OneToOne(() => CityEntity, (city) => city.tf_model)
+  city: CityEntity;
+
+  @OneToMany(() => TF_training, (train) => train.model, { eager: true })
+  train: TF_training;
 }
-
-
