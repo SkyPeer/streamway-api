fe06315f970d3f6e492224ed97142c40c2405a2d
Oleg Selivantsev <o.selivancev@gmail.com>
2025-11-30 01:21:13 +0400

    format code article

--- backend/api/src/article/dto/createArticle.dto.ts
+++ backend/api/src/article/dto/createArticle.dto.ts
@@ -1,21 +1,21 @@
-import {IsNotEmpty, IsOptional} from "class-validator";
+import { IsNotEmpty } from 'class-validator';
 // https://www.npmjs.com/package/class-validator
 // https://docs.nestjs.com/pipes
 
 export class CreateArticleDto {
-    @IsNotEmpty()
-    readonly title: string;
+  @IsNotEmpty()
+  readonly title: string;
 
-    @IsNotEmpty()
-    readonly description: string;
+  @IsNotEmpty()
+  readonly description: string;
 
-    // @IsOptional()
-    // readonly body: string;
+  // @IsOptional()
+  // readonly body: string;
 
-    @IsNotEmpty()
-    readonly body: string;
+  @IsNotEmpty()
+  readonly body: string;
 
-    readonly tagList? :string[];
+  readonly tagList?: string[];
 }
 
 // export class UpdateArticleDto {
