f7e6455d8e2481c1b4ad639436dcc81c0d2d06f5
Oleg Selivantsev <o.selivancev@gmail.com>
2025-11-30 01:42:31 +0400

    upd format

--- backend/api/src/forecast/entities/temperature.entity.ts
+++ backend/api/src/forecast/entities/temperature.entity.ts
@@ -1,34 +1,38 @@
-import {Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne} from 'typeorm';
-import {CityEntity} from "@app/forecast/entities/city.entity";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { CityEntity } from '@app/forecast/entities/city.entity';
 
 @Entity({ name: 'forecast_temperature' })
 export class ForecastTemperatureEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({type: 'decimal', precision: 10, scale: 7})
+  @Column({ type: 'decimal', precision: 10, scale: 7 })
   min: number;
 
-  @Column({type: 'decimal', precision: 10, scale: 7})
+  @Column({ type: 'decimal', precision: 10, scale: 7 })
   max: number;
 
-  @Column({type: 'decimal', precision: 10, scale: 7})
+  @Column({ type: 'decimal', precision: 10, scale: 7 })
   wind_speed: number;
 
-  @Column({type: 'decimal', precision: 10, scale: 7})
+  @Column({ type: 'decimal', precision: 10, scale: 7 })
   wind_direction: number;
 
-  @Column({type: 'decimal', precision: 10, scale: 7, nullable: true})
+  @Column({ type: 'decimal', precision: 10, scale: 7, nullable: true })
   predict: number;
 
-  @CreateDateColumn({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   timeStamp: Date;
 
   @Column()
   random: boolean;
 
-  @ManyToOne(() => CityEntity, city => city.temperatures, {eager: true})
+  @ManyToOne(() => CityEntity, (city) => city.temperatures, { eager: true })
   city: CityEntity;
 }
-
-
